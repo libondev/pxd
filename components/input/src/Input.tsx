4af279cb1b4ca5db21ceb4b04fe78f009fd78001
banlify <banlify@163.com>
2022-09-25 15:35:20 +0800

    feat: input component

---
+++ components/input/src/Input.tsx
@@ -0,0 +1,89 @@
+import '../styles/input.scss'
+
+import type { PropType } from 'vue'
+import { computed, defineComponent } from 'vue'
+
+import { SIZE } from '../../_props'
+import type { Sizes } from '../../_types/props'
+import { createClassName } from '../../_utils'
+import { useDisabled, useSizes } from '../../_utils/hooks'
+
+export default defineComponent({
+  name: 'CInput',
+  props: {
+    /**
+     * @zh 输入框值
+     */
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    },
+    /**
+     * @zh 输入框尺寸
+     */
+    size: {
+      type: String as PropType<Sizes>,
+      default: '',
+      validator: (value: Sizes) => SIZE.includes(value)
+    },
+    /**
+     * @zh 禁用
+     */
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * @zh 只读
+     */
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * @zh 是否移除展示省略号
+     */
+    ellipsis: {
+      type: Boolean,
+      default: true
+    },
+    /**
+     * @zh 自定义提示语
+     */
+    placeholder: {
+      type: String,
+      default: 'Please enter...'
+    }
+  },
+  setup (props, { slots, emit }) {
+    const innerClassName = createClassName('input__inner', [], [
+      useSizes(props).value,
+      props.ellipsis && 'c-overflow-ellipsis'
+    ])
+    const disabled = useDisabled(props)
+    const placeholder = computed(() => disabled.value || props.readonly ? '' : props.placeholder)
+
+    const modelValue = computed({
+      get () {
+        return props.modelValue
+      },
+      set (value) {
+        emit('update:modelValue', value)
+      }
+    })
+
+    return () => (
+      <div class="c-input c-inline-flex c-vertical-middle">
+        <input
+          type="text"
+          v-model={ modelValue.value }
+          tabindex="0"
+          class={ innerClassName }
+          disabled={ props.disabled }
+          readonly={ props.readonly }
+          placeholder={ placeholder.value }
+        />
+      </div>
+    )
+  }
+})
