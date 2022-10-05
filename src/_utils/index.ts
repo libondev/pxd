c368a1425d31856611832def1fb8f0587304079a
banlify <banlify@163.com>
2022-10-05 11:44:09 +0800

    feat: focus-trap

--- src/_utils/index.ts
+++ src/_utils/index.ts
@@ -1,2 +1,4 @@
+export { useFocusTrap } from './focus-trap'
 export { appendCSSUnit, createClassName } from './styles'
 export { isNull, isNullOrUndefined, isUndefined } from './typeof'
+export { createWatchers } from './watchers'
