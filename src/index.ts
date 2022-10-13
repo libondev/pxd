8d7006804060257fe5ee72bf73fce18a95724541
banlify <banlify@163.com>
2022-10-13 18:08:58 +0800

    chore: change entry file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,56 @@
+/* eslint-disable simple-import-sort/imports */
 /* eslint-disable simple-import-sort/exports */
+import type { App, Component } from 'vue'
 
-// #region
+import { globalSymbol } from './_internal'
+
+// #region import
+import { CButton } from './button'
+import { CDialog } from './dialog'
+import { CIcon } from './icon'
+import { CInput } from './input'
+import { CProvider } from './provider'
+import { CSpinner } from './spinner'
+import { CSwitch } from './switch'
+// #endregion import
+
+// #region export
 export * from './_types'
 export * from './button'
+export * from './dialog'
 export * from './icon'
 export * from './input'
 export * from './provider'
 export * from './spinner'
-export * from './dialog'
 export * from './switch'
-// #endregion
+// #endregion export
 
-export { installCarbons as default } from './registry'
+interface GlobalConfig {
+  size: import('./_types/props').Sizes
+}
+
+export default function installCarbons< VueApp extends App<string> > (
+  globalConfig?: GlobalConfig
+): (app: VueApp) => VueApp {
+  // #region registry
+  const components = [
+    CButton,
+    CIcon,
+    CInput,
+    CProvider,
+    CDialog,
+    CSpinner,
+    CSwitch
+  ] as Array<Component & { name: string }>
+  // #endregion registry
+
+  return (app: VueApp): VueApp => {
+    app.provide(globalSymbol, Object.assign({ size: 'medium' }, globalConfig))
+
+    components.forEach(component => {
+      app.component(component.name, component)
+    })
+
+    return app
+  }
+}
