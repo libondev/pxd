fe0cff641dad6b3a8399145eef75a2e2ba0512d8
banlify <banlify@163.com>
2022-10-14 20:13:11 +0800

    chore: update resolver

--- src/resolver.ts
+++ src/resolver.ts
@@ -3,11 +3,9 @@ import type { ComponentResolver, SideEffectsInfo } from 'unplugin-vue-components
 const STYLELESS = ['provider', 'spinner']
 
 function getSideEffects (name: string): SideEffectsInfo | undefined {
-  console.log('getSideEffects', name)
-
   if (STYLELESS.includes(name)) return
 
-  return ['/carbons/_styles/base.css', `/carbons/_styles/${name}.css`]
+  return ['carbons/_styles/base.css', `carbons/_styles/${name}.css`]
 }
 
 export default function CarbonsResolver (): ComponentResolver {
@@ -19,7 +17,7 @@ export default function CarbonsResolver (): ComponentResolver {
 
         return {
           importName: name,
-          from: `/carbons/${partialName}`,
+          from: `carbons/${partialName}`,
           sideEffects: getSideEffects(partialName)
         }
       }
