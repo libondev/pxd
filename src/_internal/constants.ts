3dc040135d1e4b0e8b2fb9666994bf25426b6744
banlify <banlify@163.com>
2022-10-16 11:53:36 +0800

    chore: add interactive-elements selector to constants

--- src/_internal/constants.ts
+++ src/_internal/constants.ts
@@ -1,1 +1,3 @@
 export function noop (): void { }
+
+export const INTERACTIVE_ELEMENTS = 'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])'
