9b369213be16f6329af92bac6d1cba527d7aa1ae
banlify <banlify@163.com>
2022-10-16 19:55:13 +0800

    chore: update example

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+
+const value = ref('')
+const status = ref(false)
 </script>
 
 <template>
+  <CProvider>
   <nav>
     <RouterLink tabindex="-1" to="/">Home</RouterLink> |
     <RouterLink tabindex="-1" to="/about">About</RouterLink>
   </nav>
 
-  <CButton>123</CButton>
-  <CSwitch />
+  <CSpace>
+    <!-- <CButton>123</CButton>
+    <CButton>123</CButton>
+    <CButton>123</CButton> -->
+    <CSwitch v-model="status" />
+    <CCheckbox v-model="value" :value="1">111</CCheckbox>
+    <CCheckbox v-model="value" :value="2" />
+    <CCheckbox v-model="value" :value="3" />
+  </CSpace>
   <!-- <RouterView /> -->
+</CProvider>
 </template>
