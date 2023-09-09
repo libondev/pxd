a1c36725b49c644a9defdbb3faa5ee807c167da9
Libon <bon.li@outlook.com>
2023-09-09 08:20:50 +0800

    chore: add dialog

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,20 @@ import type { App } from 'vue'
 import Button from './components/button'
 import Switch from './components/switch'
 import Checkbox from './components/checkbox'
+import Dialog from './components/dialog'
 
 export {
   Button,
   Switch,
-  Checkbox
+  Checkbox,
+  Dialog
 }
 
 const COMPONENTS = [
   Button,
   Switch,
+  Checkbox,
+  Dialog
 ]
 
 export default function (Vue: App) {
