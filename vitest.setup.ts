d8a69ce4fffabf51892b4b3c7b7c1354e5a1a6a1
libondev <bon.li@outlook.com>
2025-04-24 15:57:33 +0800

    chore: update icons version

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,33 +1,33 @@
-import { afterAll, beforeAll, beforeEach } from 'vitest'
+// import { afterAll, beforeAll, beforeEach } from 'vitest'
 
-beforeAll(() => {
-  globalThis.something = 'something'
-})
+// beforeAll(() => {
+//   globalThis.something = 'something'
+// })
 
-beforeAll(async () => {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(null)
-    }, 300)
-  })
-})
+// beforeAll(async () => {
+//   await new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(null)
+//     }, 300)
+//   })
+// })
 
-beforeEach(async () => {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(null)
-    }, 10)
-  })
-})
+// beforeEach(async () => {
+//   await new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(null)
+//     }, 10)
+//   })
+// })
 
-afterAll(() => {
-  delete globalThis.something
-})
+// afterAll(() => {
+//   delete globalThis.something
+// })
 
-afterAll(async () => {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(null)
-    }, 500)
-  })
-})
+// afterAll(async () => {
+//   await new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(null)
+//     }, 500)
+//   })
+// })
