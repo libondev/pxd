ad400858600f83e1f5e7e1cd5a6e41e806b0e7bf
libondev <bon.li@outlook.com>
2025-04-26 20:53:25 +0800

    chore: to hash router

--- packages/docs/src/router/index.ts
+++ packages/docs/src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { handleHotUpdate, routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior() {
     return {
