e66b4fee64765eaf97774413b620a1c1f641cf11
libondev <bon.li@outlook.com>
2025-05-09 15:03:45 +0800

    chore: add components totals

--- packages/docs/src/pages/components/index.vue
+++ packages/docs/src/pages/components/index.vue
@@ -8,6 +8,10 @@ import { components } from '@/consts/components'
     Overview
   </h1>
 
+  <p class="text-sm text-gray-900">
+    A total of <span class="font-medium text-primary">{{ components.length }}</span> components
+  </p>
+
   <div class="flex flex-wrap gap-5 mt-6">
     <template v-for="{ camelized, name } in components" :key="name">
       <OverviewCard :name="camelized">
