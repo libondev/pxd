e241ab9f1f3f937b7cbe42fbf3c58caff0a2aced
libondev <bon.li@outlook.com>
2025-05-13 11:05:56 +0800

    style: code style lint

--- packages/docs/src/components/SiteLogo.vue
+++ packages/docs/src/components/SiteLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <svg width="1em" height="1em" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="32" width="32" height="32" fill="currentColor"></rect>
-    <rect y="32" width="32" height="32" fill="currentColor"></rect>
+    <rect x="32" width="32" height="32" fill="currentColor" />
+    <rect y="32" width="32" height="32" fill="currentColor" />
   </svg>
 </template>
