7835f6ffd7f03674972f6cb8c7007718ce635484
libondev <bon.li@outlook.com>
2025-05-26 18:18:28 +0800

    feat: add skeleton

---
+++ packages/docs/src/pages/components/skeleton.md
@@ -0,0 +1,74 @@
+# Skeleton
+Display a skeleton whilst another component is loading.
+
+## Default with set width
+
+```vue demo
+<template>
+  <PSkeleton width="160" />
+</template>
+```
+
+## Default with box height
+
+```vue demo
+<template>
+  <PSkeleton width="160" box-height="42" />
+</template>
+```
+
+## Wrapping children
+Setting the height to `auto` automatically calculates the height.
+
+```vue demo
+<template>
+  <PStack gap="4" direction="col">
+    <PSkeleton height="auto">
+      <PButton>Hidden by skeleton</PButton>
+    </PSkeleton>
+
+    <PSkeleton :loading="false" height="auto">
+      <PButton>Not hidden by skeleton</PButton>
+    </PSkeleton>
+  </PStack>
+</template>
+```
+
+## Wrapping children with fixed size
+The skeleton will hide when children are not null, but the size is retained.
+
+```vue demo
+<template>
+  <PStack gap="4" direction="col">
+    <PSkeleton height="100" width="100%" />
+
+    <PSkeleton :loading="false" height="100" width="100%">
+      <PButton>Not hidden by skeleton</PButton>
+    </PSkeleton>
+  </PStack>
+</template>
+```
+
+## Shapes
+
+```vue demo
+<template>
+  <PStack gap="4" direction="col">
+    <PSkeleton shape="default" width="48" />
+
+    <PSkeleton shape="rounded" width="48" height="48" />
+
+    <PSkeleton shape="squared" width="48" height="48" />
+  </PStack>
+</template>
+```
+
+## No animation
+
+```vue demo
+<template>
+  <PStack gap="4" direction="col">
+    <PSkeleton height="100" width="100%" :animated="false" />
+  </PStack>
+</template>
+```
---
+++ src/components/skeleton/index.vue
@@ -0,0 +1,94 @@
+<script lang="ts" setup>
+import type { CSSProperties } from 'vue'
+import { computed } from 'vue'
+import { getCssUnitValue, increaseWithUnit } from '../../utils/format'
+
+interface Props {
+  loading?: boolean
+  animated?: boolean
+  width?: string | number
+  height?: string | number
+  boxHeight?: string | number
+  shape?: 'default' | 'squared' | 'rounded'
+}
+
+defineOptions({
+  name: 'PSkeleton',
+})
+
+const props = withDefaults(
+  defineProps<Props>(),
+  {
+    loading: true,
+    height: 24,
+    shape: 'default',
+  },
+)
+
+const computedStyle = computed(() => {
+  const { width, height, boxHeight } = props
+
+  const styles = {
+    width: getCssUnitValue(width),
+    height: getCssUnitValue(height),
+  } as CSSProperties
+
+  if (boxHeight) {
+    styles['margin-bottom'] = getCssUnitValue(increaseWithUnit(boxHeight, -Number(height)))
+  }
+
+  return styles
+})
+
+const computedClasses = computed(() => {
+  const { loading, shape, animated } = props
+  const basic = ['pxd-skeleton relative block flex-shrink-0 overflow-hidden']
+
+  if (loading) {
+    basic.push('loading invisible')
+  }
+
+  if (shape === 'rounded') {
+    basic.push('rounded-full')
+  } else if (shape === 'squared') {
+    basic.push('rounded-none')
+  } else {
+    basic.push('rounded-md')
+  }
+
+  if (animated) {
+    basic.push('animated')
+  }
+
+  return basic
+})
+</script>
+
+<template>
+  <div
+    :class="computedClasses"
+    :style="computedStyle"
+  >
+    <slot />
+  </div>
+</template>
+
+<style>
+.pxd-skeleton.loading::after {
+  content: "";
+  position: absolute;
+  inset: 0 -200% 0 0;
+  visibility: visible;
+  background: linear-gradient(90deg, var(--color-gray-100), var(--color-gray-200), var(--color-gray-100)) 0 0 / 50% 100%;
+}
+
+.pxd-skeleton.animated::after {
+  animation: skeleton-loading 1.5s ease-in-out infinite reverse;
+}
+
+@keyframes skeleton-loading {
+  100% {
+    transform: translate3d(-50%, 0, 0);
+  }
+}
+</style>
