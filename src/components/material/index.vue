3344a13bd498b7089ea22f04b7862f5f61179a8a
libondev <bon.li@outlook.com>
2025-05-29 16:20:00 +0800

    style: shadow-border css variant move to root

--- src/components/material/index.vue
+++ src/components/material/index.vue
@@ -18,65 +18,44 @@ withDefaults(
 </script>
 
 <template>
-  <PStack class="pxd-material" :class="variant">
+  <PStack class="pxd-material bg-background" :class="variant">
     <slot />
   </PStack>
 </template>
 
 <style lang="postcss">
 .pxd-material {
-  --default-shadow: 0 0 0 1px rgba(0,0,0,0.08);
-  --small-shadow: 0 0 0 1px rgba(0,0,0,0.08), 0px 2px 2px rgba(0,0,0,0.04);
-  --medium-shadow: 0 0 0 1px rgba(0,0,0,0.08), 0px 2px 2px rgba(0,0,0,0.04),0px 8px 8px -8px rgba(0,0,0,0.04);
-  --large-shadow: 0 0 0 1px rgba(0,0,0,0.08), 0px 2px 2px rgba(0,0,0,0.04),0px 8px 16px -4px rgba(0,0,0,0.04);
-  --tooltip-shadow: 0 0 0 1px rgba(0,0,0,0.08),0px 1px 1px rgba(0,0,0,0.02),0px 4px 8px rgba(0,0,0,0.04);
-  --menu-shadow: 0 0 0 1px rgba(0,0,0,0.08),0px 1px 1px rgba(0,0,0,0.02),0px 4px 8px -4px rgba(0,0,0,0.04),0px 16px 24px -8px rgba(0,0,0,0.06);
-  --modal-shadow: 0 0 0 1px rgba(0,0,0,0.08),0px 1px 1px rgba(0,0,0,0.02),0px 8px 16px -4px rgba(0,0,0,0.04),0px 24px 32px -8px rgba(0,0,0,0.06);
-  --fullscreen-shadow: 0 0 0 1px rgba(0,0,0,0.08),0px 1px 1px rgba(0,0,0,0.02),0px 8px 16px -4px rgba(0,0,0,0.04),0px 24px 32px -8px rgba(0,0,0,0.06);
-  background-color: var(--color-background);
-
   &.default {
     border-radius: 6px;
-    box-shadow: var(--default-shadow);
+    box-shadow: var(--shadow-border-default);
   }
   &.small {
     border-radius: 6px;
-    box-shadow: var(--small-shadow);
+    box-shadow: var(--shadow-border-small);
   }
   &.medium {
     border-radius: 12px;
-    box-shadow: var(--medium-shadow);
+    box-shadow: var(--shadow-border-medium);
   }
   &.large {
     border-radius: 12px;
-    box-shadow: var(--large-shadow);
+    box-shadow: var(--shadow-border-large);
   }
   &.tooltip {
     border-radius: 6px;
-    box-shadow: var(--tooltip-shadow);
+    box-shadow: var(--shadow-border-tooltip);
   }
   &.menu {
     border-radius: 12px;
-    box-shadow: var(--menu-shadow);
+    box-shadow: var(--shadow-border-menu);
   }
   &.modal {
     border-radius: 12px;
-    box-shadow: var(--modal-shadow);
+    box-shadow: var(--shadow-border-modal);
   }
   &.fullscreen {
     border-radius: 16px;
-    box-shadow: var(--fullscreen-shadow);
+    box-shadow: var(--shadow-border-fullscreen);
   }
-}
-
-.dark .pxd-material {
-  --default-shadow: 0 0 0 1px rgba(255,255,255,0.145);
-  --small-shadow: 0 0 0 1px rgba(255,255,255,0.145),0px 1px 2px rgba(0,0,0,0.16);
-  --medium-shadow: 0 0 0 1px rgba(255,255,255,0.145),0px 2px 2px rgba(0,0,0,0.32),0px 8px 8px -8px rgba(0,0,0,0.16);
-  --large-shadow: 0 0 0 1px rgba(255,255,255,0.145),0px 2px 2px rgba(0,0,0,0.04),0px 8px 16px -4px rgba(0,0,0,0.04);
-  --tooltip-shadow: 0 0 0 1px rgba(255,255,255,0.145),0px 1px 1px rgba(0,0,0,0.02),0px 4px 8px rgba(0,0,0,0.04);
-  --menu-shadow: 0 0 0 1px rgba(255,255,255,0.145),0px 1px 1px rgba(0,0,0,0.02),0px 4px 8px -4px rgba(0,0,0,0.04),0px 16px 24px -8px rgba(0,0,0,0.06);
-  --modal-shadow: 0 0 0 1px rgba(255,255,255,0.145),0px 1px 1px rgba(0,0,0,0.02),0px 8px 16px -4px rgba(0,0,0,0.04),0px 24px 32px -8px rgba(0,0,0,0.06);
-  --fullscreen-shadow: 0 0 0 1px rgba(255,255,255,0.145),0px 1px 1px rgba(0,0,0,0.02),0px 8px 16px -4px rgba(0,0,0,0.04),0px 24px 32px -8px rgba(0,0,0,0.06);
 }
 </style>
