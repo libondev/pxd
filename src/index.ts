0ba596022aa5b418970420b0dc1d5b6365f192b6
libondev <bon.li@outlook.com>
2025-05-29 10:42:00 +0800

    chore: update pkg exports

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as components from './components/index'
 
 export * from './components/index'
 
-export const version = '0.0.17'
+export const version = '0.0.18'
 
 export default function install(app: App, prefix = 'P') {
   Object.entries(components).forEach(([key, component]) => {
