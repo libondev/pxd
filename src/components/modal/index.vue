a41133575eb7125a4bfba2e09551a2fdc860fe9d
libondev <bon.li@outlook.com>
2025-07-17 15:35:08 +0800

    style(modal): modal subtitle font styles

--- src/components/modal/index.vue
+++ src/components/modal/index.vue
@@ -92,7 +92,7 @@ watch(() => isVisible.value, (visible) => {
         @keydown.esc="onModalKeydown"
       >
         <header
-          class="pxd-modal--header relative shrink-0 px-6 py-4 sm:py-6 -mb-6"
+          class="pxd-modal--header relative shrink-0 px-6 py-4 -mb-4 sm:py-6 sm:-mb-6"
           :class="{ 'mb-0 border-b bg-background-secondary dark:bg-background': headerStyle }"
         >
           <h3 class="text-base sm:text-2xl font-semibold tracking-tight">
@@ -101,7 +101,7 @@ watch(() => isVisible.value, (visible) => {
             </slot>
           </h3>
 
-          <div v-if="subtitle" class="mt-6">
+          <div v-if="subtitle" class="mt-4 text-sm text-muted-foreground">
             <slot name="subtitle">
               {{ subtitle }}
             </slot>
