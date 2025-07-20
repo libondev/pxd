0fb58675fdad59466c8bf49c682f7a58798e7c3b
libondev <bon.li@outlook.com>
2025-07-20 14:09:38 +0800

    fix: hide the view source button of other pages

--- packages/docs/src/layouts/default.vue
+++ packages/docs/src/layouts/default.vue
@@ -30,7 +30,7 @@ const route = useRoute()
 const openSidebar = ref(false)
 
 const showViewSource = computed(() => {
-  return !['/[...all]', '/components/'].includes(route.name)
+  return route.name.startsWith('/components') && !route.name.endsWith('/')
 })
 
 const flattenedMenus = computed(() => {
