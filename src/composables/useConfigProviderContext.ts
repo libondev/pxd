890166f0344dfcd03a28bcbb6f9425138b1b820b
libondev <bon.li@outlook.com>
2025-07-22 14:21:03 +0800

    refactor(config-provide): use createContext create context

--- src/composables/useConfigProviderContext.ts
+++ src/composables/useConfigProviderContext.ts
@@ -1,24 +1,21 @@
 import type { ComponentSize } from '../types/components'
-import { inject, provide } from 'vue'
-
 import enUS from '../locales/en-US'
+import { createContext } from '../utils/context'
 
-export const injectionKey = 'configProvider'
+export const injectionKey = 'ConfigProvider'
 
 export interface ConfigProviderProps {
   size?: ComponentSize
   locale?: Record<string, any>
 }
 
-export function provideConfigProvider(configProvider: ConfigProviderProps): void {
-  provide(injectionKey, configProvider)
-}
-
-export function useConfigProvider(): Required<ConfigProviderProps> {
-  const defaultConfig: Required<ConfigProviderProps> = {
+export const [
+  provideConfigProvider,
+  useConfigProvider,
+] = createContext<Required<ConfigProviderProps>>(
+  injectionKey,
+  {
     size: 'md',
     locale: enUS,
-  }
-
-  return inject(injectionKey, defaultConfig)
-}
+  },
+)
