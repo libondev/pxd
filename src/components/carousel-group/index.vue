4e32595bad9aefa74b213a53fa7c50b8a5d913d7
libondev <bon.li@outlook.com>
2025-07-23 22:02:26 +0800

    style(carousel): button focus ring style

--- src/components/carousel-group/index.vue
+++ src/components/carousel-group/index.vue
@@ -229,7 +229,7 @@ onBeforeUnmount(() => {
         v-for="(_, i) in carousels.length"
         :key="i"
         :data-index="i"
-        class="pxd-carousel-group--indicator-item w-(--w) h-(--h) relative rounded-full appearance-none cursor-pointer outline-none bg-gray-alpha-200 motion-safe:transition-colors hover:bg-gray-alpha-400"
+        class="pxd-carousel-group--indicator-item w-(--w) h-(--h) self-focus-ring relative rounded-full appearance-none cursor-pointer outline-none bg-gray-alpha-200 motion-safe:transition-colors hover:bg-gray-alpha-400"
         :class="{ '!bg-primary': i === correctIndex }"
       />
     </div>
@@ -238,7 +238,7 @@ onBeforeUnmount(() => {
       <button
         type="button"
         aria-label="Carousel arrow left"
-        class="pxd-carousel-group--prev-button group-data-[direction=vertical]:rotate-90 appearance-none p-1.5 rounded-md bg-gray-alpha-200 cursor-pointer disabled:pointer-events-none opacity-40 hover:opacity-100 active:bg-gray-alpha-400 motion-safe:transition-colors"
+        class="pxd-carousel-group--prev-button outline-none self-focus-ring group-data-[direction=vertical]:rotate-90 appearance-none p-1.5 rounded-md bg-gray-alpha-200 cursor-pointer disabled:pointer-events-none hover:bg-gray-alpha-100 hover:bg-gray-alpha-200 active:bg-gray-alpha-400 motion-safe:transition-colors"
         @click="onToggleClick(-1)"
       >
         <ChevronRightIcon class="rotate-180" />
@@ -247,7 +247,7 @@ onBeforeUnmount(() => {
       <button
         type="button"
         aria-label="Carousel arrow right"
-        class="pxd-carousel-group--next-button group-data-[direction=vertical]:rotate-90 appearance-none p-1.5 rounded-md bg-gray-alpha-200 cursor-pointer disabled:pointer-events-none opacity-40 hover:opacity-100 active:bg-gray-alpha-400 motion-safe:transition-colors"
+        class="pxd-carousel-group--next-button outline-none self-focus-ring group-data-[direction=vertical]:rotate-90 appearance-none p-1.5 rounded-md bg-gray-alpha-200 cursor-pointer disabled:pointer-events-none hover:bg-gray-alpha-100 hover:bg-gray-alpha-200 active:bg-gray-alpha-400 motion-safe:transition-colors outline-none"
         @click="onToggleClick(1)"
       >
         <ChevronRightIcon />
