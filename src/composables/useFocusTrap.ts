5442ea6d2e18bccc07df6df8fe8440c7192670b1
libondev <bon.li@outlook.com>
2025-07-24 11:32:21 +0800

    feat(useFocusTrap): update new element selectors

--- src/composables/useFocusTrap.ts
+++ src/composables/useFocusTrap.ts
@@ -3,7 +3,26 @@ import { nextTick, onBeforeUnmount, watch } from 'vue'
 import { on } from '../utils/events'
 
 export function useFocusTrap(containerRef: Ref<HTMLElement | undefined>) {
-  const selectors = ['input:not([inert])', 'select:not([inert])', 'textarea:not([inert])', 'a[href]:not([inert])', 'button:not([inert])', '[tabindex]:not(slot):not([inert])', 'audio[controls]:not([inert])', 'video[controls]:not([inert])', '[contenteditable]:not([contenteditable="false"]):not([inert])', 'details>summary:first-of-type:not([inert])', 'details:not([inert])']
+  const FOCUSABLE_SELECTORS = [
+    ':focus',
+    'a[href]:not([tabindex^="-"])',
+    'area[href]:not([tabindex^="-"])',
+    'video[controls]:not([tabindex^="-"])',
+    'audio[controls]:not([tabindex^="-"])',
+    'iframe:not([tabindex^="-"])',
+    '[tabindex]:not(slot):not([tabindex^="-"])',
+    '[contenteditable]:not([contenteditable="false"]):not([tabindex^="-"])',
+    'details > summary:first-of-type:not([tabindex^="-"])',
+    'input:not([type="hidden"]):not(fieldset[disabled] input):not([disabled]):not([tabindex^="-"])',
+    'select:not(fieldset[disabled] input):not([disabled]):not([tabindex^="-"])',
+    'textarea:not(fieldset[disabled] input):not([disabled]):not([tabindex^="-"])',
+    'button:not(fieldset[disabled] input):not([disabled]):not([tabindex^="-"])',
+    'fieldset[disabled]:not(fieldset[disabled] fieldset) > legend input:not([type="hidden"]):not([disabled]):not([tabindex^="-"])',
+    'fieldset[disabled]:not(fieldset[disabled] fieldset) > legend select:not([disabled]):not([tabindex^="-"])',
+    'fieldset[disabled]:not(fieldset[disabled] fieldset) > legend textarea:not([disabled]):not([tabindex^="-"])',
+    'fieldset[disabled]:not(fieldset[disabled] fieldset) > legend button:not([disabled]):not([tabindex^="-"])',
+    '[class*="focusable"]:not([disabled]):not([tabindex^="-"])',
+  ].join(',')
 
   let elements: HTMLElement[] = []
   let previousFocusedElement: HTMLElement | null = null
@@ -32,7 +51,7 @@ export function useFocusTrap(containerRef: Ref<HTMLElement | undefined>) {
 
     nextTick(() => {
       const unbindEvent = on(container, 'keydown', onContainerKeydown)
-      elements = Array.from(container.querySelectorAll<HTMLElement>(selectors.join(',')))
+      elements = Array.from(container.querySelectorAll<HTMLElement>(FOCUSABLE_SELECTORS))
 
       if (elements.length) {
         elements[0].focus()
