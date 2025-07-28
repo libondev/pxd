dc34c4f3fc0b4b89c40308321609139dc0a3e67a
libondev <bon.li@outlook.com>
2025-07-28 16:47:18 +0800

    feat(toggle): toggle support set label and slot

--- src/components/toggle/index.vue
+++ src/components/toggle/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { ComponentSize } from '../../types/shared'
+import type { ComponentLabel, ComponentSize } from '../../types/shared'
 import { computed } from 'vue'
 import { useConfigProvider } from '../../composables/useConfigProviderContext'
 import { useModelValue } from '../../composables/useModelValue'
@@ -10,6 +10,7 @@ type ValueType = boolean | number | string
 
 interface Props {
   size?: ComponentSize
+  label?: ComponentLabel
   modelValue?: ValueType
   activeValue?: ValueType
   inactiveValue?: ValueType
@@ -64,7 +65,7 @@ function onCheckboxChange(e: Event) {
 <template>
   <label
     role="switch"
-    class="pxd-toggle group/toggle inline-flex items-center cursor-pointer select-none"
+    class="pxd-toggle group/toggle inline-flex flex-col cursor-pointer select-none"
     :aria-label="modelValue ? activeLabel : inactiveLabel"
     :style="{
       '--abc': activeBgColor,
@@ -72,32 +73,38 @@ function onCheckboxChange(e: Event) {
     }"
     :for="uniqueId"
   >
-    <input
-      :id="uniqueId"
-      type="checkbox"
-      :checked="isChecked"
-      class="smallest peer"
-      @change="onCheckboxChange"
-    >
-
-    <span
-      v-if="inactiveLabel"
-      class="pxd-toggle--label motion-safe:transition-opacity opacity-100 peer-checked:opacity-50 mr-1.5 pl-0.5"
-    >{{ inactiveLabel }}</span>
-
-    <div
-      class="pxd-toggle--handle border border-input p-px rounded-full motion-safe:transition-all [--tx:0] bg-(--ibc) peer-focus-ring peer-checked:bg-(--abc) peer-checked:[--tx:100%]"
-      :class="computedSize"
-    >
-      <span class="pxd-toggle--handle-icon flex items-center justify-center bg-background aspect-square h-full rounded-full border border-input transform-gpu translate-x-(--tx) motion-safe:transition-transform group-hover/toggle:will-change-transform">
-        <slot v-if="modelValue" name="active-icon" />
-        <slot v-else name="inactive-icon" />
-      </span>
+    <div v-if="label || $slots.label" class="pxd-form--label">
+      <slot name="label">{{ label }}</slot>
     </div>
 
-    <span
-      v-if="activeLabel"
-      class="pxd-toggle--label motion-safe:transition-opacity opacity-50 peer-checked:opacity-100 ml-1.5 pr-0.5"
-    >{{ activeLabel }}</span>
+    <div class="flex items-center">
+      <input
+        :id="uniqueId"
+        type="checkbox"
+        :checked="isChecked"
+        class="smallest peer"
+        @change="onCheckboxChange"
+      >
+
+      <span
+        v-if="inactiveLabel"
+        class="pxd-toggle--label text-sm opacity-100 peer-checked:opacity-50 mr-1.5 pl-0.5 motion-safe:transition-opacity"
+      >{{ inactiveLabel }}</span>
+
+      <div
+        class="pxd-toggle--handle border border-input p-px rounded-full motion-safe:transition-all [--tx:0] bg-(--ibc) peer-focus-ring peer-checked:bg-(--abc) peer-checked:[--tx:100%]"
+        :class="computedSize"
+      >
+        <span class="pxd-toggle--handle-icon flex items-center justify-center bg-background aspect-square h-full rounded-full border border-input transform-gpu translate-x-(--tx) motion-safe:transition-transform group-hover/toggle:will-change-transform">
+          <slot v-if="modelValue" name="active-icon" />
+          <slot v-else name="inactive-icon" />
+        </span>
+      </div>
+
+      <span
+        v-if="activeLabel"
+        class="pxd-toggle--label text-sm opacity-50 peer-checked:opacity-100 ml-1.5 pr-0.5 motion-safe:transition-opacity"
+      >{{ activeLabel }}</span>
+    </div>
   </label>
 </template>
