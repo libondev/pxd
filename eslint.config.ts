1860b07ae4fed6c9944dab443d09d94e8e36aa19
libondev <bon.li@outlook.com>
2025-07-31 11:34:43 +0800

    feat(eslint): add better-tailwindcss eslint rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,43 @@
 import antfu from '@antfu/eslint-config'
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 
-export default antfu({
-  ignores: [
-    'dist',
-    'node_modules/*',
-  ],
+export default antfu(
+  {
+    ignores: [
+      'dist',
+      'node_modules/*',
+    ],
 
-  test: true,
-  jsonc: true,
-  markdown: false,
-  typescript: true,
-  vue: true,
+    test: true,
+    jsonc: true,
+    markdown: false,
+    typescript: true,
+    vue: true,
 
-  rules: {
-    'curly': ['error', 'all'],
-    'vue/custom-event-name-casing': ['error', 'kebab-case'],
-    'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    rules: {
+      'curly': ['error', 'all'],
+      'vue/custom-event-name-casing': ['error', 'kebab-case'],
+      'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    },
   },
-})
+  {
+    plugins: {
+      'better-tailwindcss': eslintPluginBetterTailwindcss,
+    },
+    rules: {
+      'better-tailwindcss/enforce-consistent-class-order': ['error', { order: 'improved' }],
+      'better-tailwindcss/no-conflicting-classes': 'error',
+      'better-tailwindcss/no-duplicate-classes': 'error',
+      'better-tailwindcss/no-unnecessary-whitespace': 'error',
+      'better-tailwindcss/no-deprecated-classes': 'error',
+      'better-tailwindcss/enforce-consistent-variable-syntax': ['error', { syntax: 'shorthand' }],
+      'better-tailwindcss/enforce-consistent-important-position': ['error', { position: 'recommended' }],
+      'better-tailwindcss/enforce-shorthand-classes': 'error',
+    },
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'src/styles/tw.css',
+      },
+    },
+  },
+)
