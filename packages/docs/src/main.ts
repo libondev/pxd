45285f6bc6d8d06518140832d3c6d6279d74219a
libondev <bon.li@outlook.com>
2025-08-01 23:36:27 +0800

    chore: remove ssg related dependencies

--- packages/docs/src/main.ts
+++ packages/docs/src/main.ts
@@ -9,27 +9,3 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
-// export const createApp = ViteSSG(
-//   App,
-//   {
-//     routes: setupLayouts(routes),
-//     base: import.meta.env.BASE_URL,
-//     scrollBehavior(to, from, savedPosition) {
-//       if (savedPosition) {
-//         return savedPosition
-//       }
-
-//       if (to.hash) {
-//         return {
-//           el: to.hash,
-//           behavior: 'smooth',
-//         }
-//       }
-
-//       return {
-//         top: 0,
-//         left: 0,
-//       }
-//     },
-//   },
-// )
