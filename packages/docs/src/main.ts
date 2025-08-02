51f8b4a654bd85671ed74ef34e8a5751212f3930
libondev <bon.li@outlook.com>
2025-08-02 11:04:22 +0800

    fix: unset head title

--- packages/docs/src/main.ts
+++ packages/docs/src/main.ts
@@ -1,3 +1,4 @@
+import { createHead } from '@unhead/vue/client'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -5,7 +6,9 @@ import router from './router'
 import './styles/index.css'
 
 const app = createApp(App)
+const head = createHead()
 
+app.use(head)
 app.use(router)
 
 app.mount('#app')
