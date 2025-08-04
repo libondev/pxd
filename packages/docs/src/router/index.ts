9cda5c3f88ddac1db14a910344b9dc8ed368467c
libondev <bon.li@outlook.com>
2025-08-04 09:54:24 +0800

    fix: lost page scrolling position

--- packages/docs/src/router/index.ts
+++ packages/docs/src/router/index.ts
@@ -5,7 +5,18 @@ import { handleHotUpdate, routes } from 'vue-router/auto-routes'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: setupLayouts(routes),
-  scrollBehavior() {
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+      }
+    }
+
     return {
       top: 0,
       left: 0,
