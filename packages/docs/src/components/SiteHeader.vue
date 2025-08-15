575dfdbb9ac428d0bfce2d0ac812ea9a0d4e5485
libondev <bon.li@outlook.com>
2025-08-15 10:38:06 +0800

    style(pagination): focus ring style

--- packages/docs/src/components/SiteHeader.vue
+++ packages/docs/src/components/SiteHeader.vue
@@ -22,7 +22,7 @@ const menus = [
   <header class="top-0 sm:border-t-0 sticky z-10 border-y bg-background-100 select-none">
     <div class="md:max-w-screen-2xl h-12 mx-auto flex w-full max-w-full items-center">
       <h2 class="sm:w-60 sm:border-r h-full border-l">
-        <RouterLink to="/" class="px-3 font-medium flex h-full cursor-pointer items-center">
+        <RouterLink to="/" class="px-3 font-medium flex h-full cursor-pointer items-center self-focus-ring outline-none">
           <SiteLogo class="mr-2 text-2xl" />
           <span>PXD</span>
         </RouterLink>
