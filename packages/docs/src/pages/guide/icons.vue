62c1370fe4f4d31fa4496dd65338f7eaee6b4015
libondev <bon.li@outlook.com>
2025-08-15 10:46:52 +0800

    feat: icon page support toggle quote types

--- packages/docs/src/pages/guide/icons.vue
+++ packages/docs/src/pages/guide/icons.vue
@@ -11,9 +11,16 @@ const route = useRoute()
 const { copyText } = useCopyClick()
 
 const copyType = ref<'name' | 'import'>('import')
+const quoteType = ref<'single' | 'double' | 'single-hight'>('single')
 const searchKeyword = ref(route.query.q as string)
 
 const filteredComponents = shallowRef(getFilteredComponents(searchKeyword.value))
+
+const quoteTypeMap = {
+  'single': '\'',
+  'double': '"',
+  'single-hight': '`',
+}
 
 function getFilteredComponents(value: string) {
   if (!value) {
@@ -43,50 +50,54 @@ const onIconClick = throttle(async (ev: MouseEvent) => {
 
   if (copyType.value === 'import') {
     const filename = uncapitalize(iconName.replace('Icon', ''))
-    contents = `import ${iconName} from '@gdsicon/vue/${filename}'`
+    const quote = quoteTypeMap[quoteType.value] || '\''
+    contents = `import ${iconName} from ${quote}@gdsicon/vue/${filename}${quote}`
   }
 
   await copyText(contents)
-  useMessage(`Copied successful`, { type: 'success' })
+  useMessage.success(`Copied successful:\n${contents}`)
 }, 300)
 </script>
 
 <template>
-  <div class="">
-    <h1 class="text-2xl font-medium">
-      Icons
-    </h1>
+  <h1 class="text-2xl font-medium">
+    Icons
+  </h1>
 
-    <PText secondary class="mt-2">
-      A total of <span class="font-medium text-foreground">{{ iconCount }}</span> icons
-    </PText>
+  <PText secondary class="mt-2">
+    A total of <span class="font-medium text-foreground">{{ iconCount }}</span> icons
+  </PText>
 
-    <div class="py-4 z-10 border-b bg-background-100">
-      <PInput
-        v-model="searchKeyword"
-        placeholder="Search icons"
-        allow-clear
-        @update:model-value="handleSearch"
-      />
-    </div>
-
-    <div class="mt-4 flex justify-end">
-      <PSwitchGroup v-model="copyType">
-        <PSwitch label="Only name" value="name" />
-        <PSwitch label="Full import" value="import" />
-      </PSwitchGroup>
-    </div>
-
-    <ul class="py-4 !px-0 gap-4 md:grid-cols-[repeat(auto-fill,minmax(11rem,1fr))] grid grid-cols-2" @click="onIconClick">
-      <li v-for="{ name, icon } of filteredComponents" :key="name" :data-name="name" class="icon-item align-center !m-0 p-4 flex cursor-default list-none flex-col justify-center overflow-hidden rounded-lg border text-center select-none hover:bg-background-200 active:bg-background-hover motion-safe:transition-colors">
-        <Component :is="icon" class="my-2 mx-auto" />
-
-        <p class="!m-0 pt-2 truncate text-[13px] text-foreground-secondary">
-          {{ name }}
-        </p>
-      </li>
-    </ul>
-
-    <PMessage />
+  <div class="py-4 z-10 border-b bg-background-100">
+    <PInput
+      v-model="searchKeyword"
+      placeholder="Search icons"
+      allow-clear
+      @update:model-value="handleSearch"
+    />
   </div>
+
+  <div class="mt-4 gap-4 flex">
+    <PSwitchGroup v-model="copyType">
+      <PSwitch label="Full import" value="import" />
+      <PSwitch label="Only name" value="name" />
+    </PSwitchGroup>
+    <PSwitchGroup v-model="quoteType">
+      <PSwitch label="'" value="single" />
+      <PSwitch label="&quot;" value="double" />
+      <PSwitch label="&quot;" value="single-hight" />
+    </PSwitchGroup>
+  </div>
+
+  <ul class="py-4 !px-0 gap-4 md:grid-cols-[repeat(auto-fill,minmax(11rem,1fr))] grid grid-cols-2" @click="onIconClick">
+    <li v-for="{ name, icon } of filteredComponents" :key="name" :data-name="name" class="icon-item align-center !m-0 p-4 flex cursor-default list-none flex-col justify-center overflow-hidden rounded-lg border text-center select-none hover:bg-background-200 active:bg-background-hover motion-safe:transition-colors">
+      <Component :is="icon" class="my-2 mx-auto" />
+
+      <p class="!m-0 pt-2 truncate text-[13px] text-foreground-secondary">
+        {{ name }}
+      </p>
+    </li>
+  </ul>
+
+  <PMessage />
 </template>
