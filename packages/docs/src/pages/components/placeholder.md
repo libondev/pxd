b9265956a00044c8c0ecc9487cd247a485daef27
libondev <bon.li@outlook.com>
2025-08-17 10:49:54 +0800

    feat: add placeholder component

---
+++ packages/docs/src/pages/components/placeholder.md
@@ -0,0 +1,19 @@
+# Placeholder
+Indicates that it is not empty here.
+
+## Default
+
+```vue demo
+<template>
+  <PPlaceholder></PPlaceholder>
+</template>
+```
+
+## Custom
+Customize its size and line color.
+
+```vue demo
+<template>
+  <PPlaceholder color="#8e51ff" height="160px" width="160px" class="border-blue-900"></PPlaceholder>
+</template>
+```
---
+++ src/components/placeholder/index.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { getCssUnitValue } from '../../utils/format'
+
+interface Props {
+  color?: string
+  width?: string
+  height?: string
+}
+
+defineOptions({
+  name: 'PPlaceholder',
+  inheritAttrs: false,
+})
+
+const props = defineProps<Props>()
+
+const computedStyle = computed(() => {
+  return {
+    color: props.color,
+    width: getCssUnitValue(props.width),
+    height: getCssUnitValue(props.height),
+  }
+})
+</script>
+
+<template>
+  <div
+    class="pxd-placeholder relative max-w-full overflow-hidden rounded-lg border border-dashed"
+    :style="computedStyle"
+    v-bind="$attrs"
+  />
+</template>
+
+<style scoped>
+.pxd-placeholder {
+  height: 36px;
+  color: var(--color-gray-400);
+  background-image:
+    repeating-linear-gradient(
+      45deg,
+      currentColor 0,
+      currentColor 1px,
+      transparent 1px,
+      transparent 8px
+    );
+  background-repeat: repeat;
+  background-position: 0 0;
+  background-origin: padding-box;
+}
+</style>
