6337b1e8ed3c1e7b5e053644cacf5425fe5f1722
libondev <bon.li@outlook.com>
2025-08-20 20:43:06 +0800

    style: style optimization

--- src/components/list/index.vue
+++ src/components/list/index.vue
@@ -206,7 +206,7 @@ onBeforeUnmount(() => {
     :style="computedStyle"
     @pointerover="onPointerOver"
   >
-    <PScrollable class="max-h-68" content-class="pr-2">
+    <PScrollable class="max-h-68 p-2 rounded-inherit" content-class="" fader-direction="vertical">
       <slot>
         <PListItem
           v-for="(option, index) in options"
--- src/components/menu/index.vue
+++ src/components/menu/index.vue
@@ -56,7 +56,7 @@ function onOptionClick(ev: MouseEvent, index: number) {
       <PList
         :width="width"
         :options="options"
-        class="p-2 pr-0 list-none rounded-xl bg-background-100 shadow-border-menu outline-none"
+        class="list-none rounded-xl bg-background-100 shadow-border-menu outline-none"
         @close="closePopover"
         @selected="onOptionClick"
       >
