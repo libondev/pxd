3e431f26f7dad04ea15c5194210284654d7c741a
libondev <bon.li@outlook.com>
2025-09-11 17:26:20 +0800

    feat(composables): add use-outside-click composable

---
+++ src/composables/use-outside-click.ts
@@ -0,0 +1,55 @@
+import type { MaybeElementRef } from '../types/shared/utils'
+import { onBeforeUnmount, watch } from 'vue'
+import { optimizedOff, optimizedOn } from '../utils/event'
+import { toValue } from '../utils/ref'
+
+interface Options {
+  isEnabled?: (ev: PointerEvent) => boolean
+  isOutside?: (ev: PointerEvent) => boolean
+  onTrigger?: (ev: PointerEvent) => void
+}
+
+export function useOutsideClick(
+  container: MaybeElementRef<HTMLElement>,
+  options: Options = {},
+) {
+  function onClick(ev: PointerEvent) {
+    const { isEnabled, isOutside, onTrigger } = options
+
+    if (typeof isEnabled === 'function' && !isEnabled(ev)) {
+      return
+    }
+
+    if (typeof isOutside === 'function' && !isOutside(ev)) {
+      return
+    }
+
+    if (!toValue(container)?.contains(ev.target as HTMLElement)) {
+      return
+    }
+
+    onTrigger?.(ev)
+  }
+
+  const unwatch = watch(() => toValue(container), (dom, oldDom) => {
+    if (!dom && oldDom) {
+      optimizedOff(document, 'click', onClick)
+      return
+    }
+
+    optimizedOn(document, 'click', onClick)
+  }, { immediate: true })
+
+  function stop() {
+    unwatch()
+    optimizedOff(document, 'click', onClick)
+  }
+
+  onBeforeUnmount(() => {
+    stop()
+  })
+
+  return {
+    stop,
+  }
+}
