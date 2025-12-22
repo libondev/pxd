b2737c3a76c0989b04698c794ecc361b922186df
libondev <bon.li@outlook.com>
2025-12-22 10:53:07 +0800

    style(fader): dodge fader default color

--- src/components/fader/index.vue
+++ src/components/fader/index.vue
@@ -32,6 +32,7 @@ const fader = shallowRef({
   bottom: false,
 })
 
+const DEFAULT_SIZE = 16
 const DIFF_THRESHOLD = 1
 
 const computedStyle = computed(() => ({
@@ -56,7 +57,7 @@ const onContainerScroll = throttleByRaf(() => {
     return
   }
 
-  const { size = 16 } = props
+  const { size = DEFAULT_SIZE } = props
   const { scrollLeft, scrollWidth, clientWidth, scrollTop, clientHeight, scrollHeight } = container
 
   fader.value = {
@@ -105,8 +106,8 @@ onBeforeUnmount(() => {
   content: '';
   position: absolute;
   border-radius: inherit;
-  background: linear-gradient(var(--dir), transparent, var(--fader-color, var(--color-gray-200)));
-  mask-image: linear-gradient(var(--dir-revert), var(--fader-color, var(--color-gray-200)) 50%, transparent);
+  background: linear-gradient(var(--dir), transparent, var(--fader-color, var(--color-gray-100)));
+  mask-image: linear-gradient(var(--dir-revert), var(--fader-color, var(--color-gray-100)) 50%, transparent);
   transition: opacity var(--default-transition-duration) var(--default-transition-timing-function);
   opacity: 0;
 }
