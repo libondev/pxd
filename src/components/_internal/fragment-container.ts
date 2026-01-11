974bde31677dedaf3344fd0957005b86c5bddc27
libondev <bon.li@outlook.com>
2026-01-11 09:15:38 +0800

    refactor(fragment-container): simplified impl logic

--- src/components/_internal/fragment-container.ts
+++ src/components/_internal/fragment-container.ts
@@ -1,5 +1,5 @@
-import type { ComponentPublicInstance } from 'vue'
-import { defineComponent, h, version } from 'vue'
+import type { ComponentPublicInstance, h } from 'vue'
+import { defineComponent, version } from 'vue'
 
 interface RenderContext {
   data: Record<string, any>
@@ -12,24 +12,7 @@ export default defineComponent({
   render: (() => {
     if (version.startsWith('2')) {
       return function (_: typeof h, context: RenderContext) {
-        const slots = context.slots().default
-
-        if (!slots || !slots.length) {
-          return null
-        }
-
-        let renderAs = slots[0]!
-
-        // if there are multiple children, wrap them in a div,
-        // avoid multiple children error
-        if (slots.length > 1) {
-          renderAs = h('div', { class: 'pxd-fragment-container', ref: context.data?.ref }, slots)
-        } else if (context.data?.ref && !renderAs.data?.ref) {
-          // forward the ref to the first child
-          renderAs.data.ref = context.data?.ref
-        }
-
-        return renderAs
+        return context.slots().default
       }
     }
 
