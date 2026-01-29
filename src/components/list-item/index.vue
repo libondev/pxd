349f72ac425e23a4b9f3219a76440896479dd8d7
libondev <bon.li@outlook.com>
2026-01-29 10:40:54 +0800

    refactor(list-item): refactor classes to use tailwind-variant

--- src/components/list-item/index.vue
+++ src/components/list-item/index.vue
@@ -4,6 +4,7 @@ import { computed, nextTick, onMounted, shallowRef } from 'vue'
 import { useListContext, useListFilterValue } from '../../contexts/list'
 import { unrefElement } from '../../utils/ref'
 import { getUniqueId } from '../../utils/uid'
+import { listItemVariant } from './cn'
 
 interface Props {
   as?: ListOption['as']
@@ -41,27 +42,11 @@ const filterValue = useListFilterValue()
 const itemRef = shallowRef<HTMLElement>()
 const currentValue = shallowRef('')
 
-const itemTypeMap = {
-  error: 'text-red-900 pointer-coarse:active:bg-red-100 pointer-fine:data-[selected=true]:bg-red-100',
-  warning: 'text-amber-900 pointer-coarse:active:bg-amber-100 pointer-fine:data-[selected=true]:bg-amber-100',
-  default: 'text-foreground pointer-coarse:active:bg-gray-alpha-100 pointer-fine:data-[selected=true]:bg-gray-alpha-100',
-  separator: '!h-0 !w-auto px-0 m-1.5 border-b',
-}
-
 const isVisible = computed(() => filterValue?.value ? currentValue.value.includes(filterValue.value.toLowerCase()) : true)
 const isSelected = computed(() => activeValue.value === uniqueId)
 const isDisabled = computed(() => props.disabled || props.type === 'separator')
 
-const computedClass = computed(() => {
-  const { type = 'default' } = props
-  const classes = ['pxd-list-item h-10 gap-3 px-2 text-sm flex w-full cursor-pointer items-center rounded-md outline-none data-[disabled=true]:pointer-events-none data-[disabled=true]:text-gray-700 group-data-[transition=true]/list:motion-safe:transition-colors']
-
-  if (type in itemTypeMap) {
-    classes.push(itemTypeMap[type])
-  }
-
-  return classes.join(' ')
-})
+const listItemClasses = computed(() => listItemVariant({ type: props.type }))
 
 function onItemClick(ev: MouseEvent) {
   emits('click', props, ev)
@@ -90,7 +75,7 @@ onMounted(async () => {
     :data-value="uniqueId"
     :data-selected="isSelected"
     :data-disabled="isDisabled"
-    :class="computedClass"
+    :class="listItemClasses"
     @click.prevent.stop="onItemClick"
   >
     <slot v-if="type !== 'separator'">
