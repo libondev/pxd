91049057db1a96d6e15135d9440cc7f7dbbf301f
libondev <bon.li@outlook.com>
2026-01-29 11:07:54 +0800

    refactor(snippet): refactor classes to use tailwind-variant

--- src/components/snippet/index.vue
+++ src/components/snippet/index.vue
@@ -6,8 +6,8 @@ import CopyIcon from '@gdsicon/vue/copy'
 import { computed } from 'vue'
 import { useConfigProvider } from '../../composables/use-config-provider-context'
 import { useCopyClick } from '../../composables/use-copy-click'
-import { getCssUnitValue, toArray } from '../../utils/format'
-import { getFallbackValue } from '../../utils/get'
+import { getCssUnitValue, isTruthyProp, toArray } from '../../utils/format'
+import { snippetVariant } from './cn'
 
 interface Props {
   text: string | string[]
@@ -33,41 +33,20 @@ const emits = defineEmits<{
   copy: [string]
 }>()
 
-const SIZES = {
-  sm: 'min-h-7.5 pl-3.5 pr-1.5 py-2 text-sm',
-  md: 'min-h-9 pl-3.5 pr-2.5 py-2.5 text-sm',
-  lg: 'min-h-10 pl-3.5 pr-3.5 py-3 text-base',
-}
-
-const VARIANTS = {
-  default: 'border-gray-alpha-300 bg-background-100',
-  primary: 'text-gray-100 border-gray-alpha-300 bg-primary',
-  success: 'text-blue-900 border-gray-alpha-300 bg-blue-200',
-  error: 'text-red-900 border-gray-alpha-300 bg-red-200',
-  warning: 'text-amber-900 border-gray-alpha-300 bg-amber-200',
-}
-
 const config = useConfigProvider()
 
 const { isCopied, copyText } = useCopyClick()
 
 const renderIcon = computed<Component>(() => isCopied.value ? CheckIcon : CopyIcon)
-
-const computedClass = computed(() => {
-  const classes = [
-    'pxd-snippet pr-12 relative flex items-center rounded-lg border motion-safe:transition-all',
-    getFallbackValue(props.variant, VARIANTS),
-    getFallbackValue(props.size, SIZES, config.size),
-  ]
-
-  if (props.prompt) {
-    classes.push('pxd-snippet--prompt')
-  }
-
-  return classes.join(' ')
-})
-
 const computedTextArray = computed(() => toArray(props.text))
+
+const snippetClasses = computed(() => {
+  return snippetVariant({
+    size: props.size || config.size,
+    variant: props.variant,
+    prompt: isTruthyProp(props.prompt),
+  })
+})
 
 async function onCopyButtonClick() {
   const text = computedTextArray.value.join('\n')
@@ -79,7 +58,7 @@ async function onCopyButtonClick() {
 </script>
 
 <template>
-  <div :class="computedClass" :style="{ width: getCssUnitValue(props.width) }">
+  <div :class="snippetClasses" :style="{ width: getCssUnitValue(props.width) }">
     <div class="pxd-snippet--container">
       <pre v-for="(t, i) of computedTextArray" :key="i" class="m-0 p-0" :data-prompt="prompt" :class="{ 'before:content-[attr(data-prompt)] before:select-none': prompt }">{{ t }}</pre>
     </div>
