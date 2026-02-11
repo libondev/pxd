b93083d516386795cc2685074de87f83dc4953b8
libondev <bon.li@outlook.com>
2026-02-11 14:31:38 +0800

    style: switch padding

--- src/components/switch/cn.ts
+++ src/components/switch/cn.ts
@@ -1,7 +1,7 @@
 import { tv } from 'tailwind-variants'
 
 export const switchVariant = tv({
-  base: 'pxd-switch--label px-3 text-sm font-medium flex size-full items-center justify-center truncate rounded-sm text-foreground-secondary peer-focus-ring select-none peer-checked:bg-gray-100 peer-disabled:cursor-not-allowed peer-disabled:text-gray-800 empty:hidden motion-safe:transition-all',
+  base: 'pxd-switch--label px-2.5 text-sm font-medium flex size-full items-center justify-center truncate rounded-sm text-foreground-secondary peer-focus-ring select-none peer-checked:bg-gray-100 peer-disabled:cursor-not-allowed peer-disabled:text-gray-800 empty:hidden hover:text-foreground motion-safe:transition-all',
   variants: {
     disabled: {
       true: '',
