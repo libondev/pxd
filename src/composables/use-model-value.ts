dd0e2c0dfd7e2123cbdc051c549bc1564d0d7652
libondev <bon.li@outlook.com>
2026-02-13 09:16:27 +0800

    chore: update use-model-value types

--- src/composables/use-model-value.ts
+++ src/composables/use-model-value.ts
@@ -2,21 +2,25 @@ import type { WritableComputedRef } from 'vue'
 
 import { computed } from 'vue'
 
-interface Options {
+interface BaseProps {
+  modelValue?: any
+}
+
+interface BaseEmits {
+  (event: 'change', ...args: any[]): void
+  (event: 'update:modelValue', ...args: any[]): void
+}
+
+interface ModelValueOptions {
   get?: (value: any) => any
   set?: (value: any) => void
   withChange?: boolean
 }
 
-interface BaseEmit {
-  (event: 'change', ...args: any[]): void
-  (event: 'update:modelValue', ...args: any[]): void
-}
-
-export function useModelValue<P extends { modelValue?: any }, E extends BaseEmit>(
+export function useModelValue<P extends BaseProps, E extends BaseEmits>(
   props: P,
   emits: E,
-  options: Options = { withChange: true },
+  options: ModelValueOptions = { withChange: true },
 ): WritableComputedRef<NonNullable<P['modelValue']>> {
   type V = NonNullable<P['modelValue']>
 
