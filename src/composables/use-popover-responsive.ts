4d4796e6517045b40a237c03e8000a5368959f1c
Libon <bon.li@outlook.com>
2026-04-03 14:08:15 +0800

    style: remove popover border on adaptive

--- src/composables/use-popover-responsive.ts
+++ src/composables/use-popover-responsive.ts
@@ -5,17 +5,15 @@ export function usePopoverResponsive() {
   const isAdaptive = useMediaQuery(PRESET_MEDIA_QUERIES.IS_XS)
 
   const responsiveClasses = computed(() => {
-    const basicContentClass = 'bg-background-100 border'
-
     if (isAdaptive.value) {
       return {
-        contentClass: `${basicContentClass} w-full rounded-tl-xl rounded-tr-xl`,
+        contentClass: `bg-background-100 w-full rounded-tl-xl rounded-tr-xl`,
         wrapperClass: 'fixed w-screen h-screen items-end pointer-events-none pxd-container-mask',
       } as const
     }
 
     return {
-      contentClass: `${basicContentClass} rounded-xl`,
+      contentClass: `bg-background-100 border rounded-xl`,
       wrapperClass: '',
     } as const
   })
