d0a6bcb82ba2566e520cd2222ace47cec8080d7f
Libon <bon.li@outlook.com>
2026-07-14 09:59:04 +0800

    chore: improve use-view-transition composables

--- packages/docs/src/router/use-view-transition.ts
+++ packages/docs/src/router/use-view-transition.ts
@@ -5,28 +5,50 @@ export function useViewTransition(router: Router) {
     return
   }
 
+  let activeTransition: ViewTransition | undefined
   let finishTransition: undefined | (() => void)
   let abortTransition: undefined | (() => void)
 
+  function skipActiveTransition() {
+    activeTransition?.skipTransition()
+    finishTransition?.()
+
+    activeTransition = undefined
+    finishTransition = undefined
+    abortTransition = undefined
+  }
+
   router.beforeResolve(() => {
+    skipActiveTransition()
+
     const promise = new Promise<void>((resolve, reject) => {
       finishTransition = resolve
       abortTransition = reject
     })
 
     let changeRoute: () => void
-    const ready = new Promise<void>((resolve) => (changeRoute = resolve))
+    const ready = new Promise<void>((resolve) => {
+      changeRoute = resolve
+    })
 
     const transition = document.startViewTransition!(() => {
       changeRoute()
-
       return promise
     })
 
-    void transition.finished.then(() => {
-      abortTransition = undefined
-      finishTransition = undefined
-    })
+    activeTransition = transition
+
+    void transition.finished
+      .catch(() => {})
+      .then(() => {
+        if (activeTransition !== transition) {
+          return
+        }
+
+        activeTransition = undefined
+        finishTransition = undefined
+        abortTransition = undefined
+      })
 
     return ready
   })
