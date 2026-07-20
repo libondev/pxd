dcc7b14aeccab9006915921f5fd9409cd9b7b407
Libon <bon.li@outlook.com>
2026-07-20 11:49:06 +0800

    fix(separator): use borders for consistent divider rendering

--- src/components/separator/index.vue
+++ src/components/separator/index.vue
@@ -15,7 +15,7 @@ const props = withDefaults(defineProps<SeparatorProps>(), {
   <div
     role="separator"
     :aria-orientation="direction"
-    class="pxd-separator aria-[orientation=vertical]:h-3 inline-block bg-gray-300 leading-inherit aria-[orientation=horizontal]:h-px aria-[orientation=horizontal]:w-full aria-[orientation=vertical]:w-px"
+    class="pxd-separator aria-[orientation=vertical]:h-3 box-border inline-block shrink-0 border-0 border-gray-300 leading-inherit aria-[orientation=horizontal]:w-full aria-[orientation=horizontal]:border-t aria-[orientation=vertical]:border-l aria-[orientation=vertical]:border-gray-500"
     v-bind="$attrs"
   />
 </template>
