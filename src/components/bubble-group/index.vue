bcdc82bcce3a85efb92f3a5f8271ba11c253ecaa
Libon <bon.li@outlook.com>
2026-09-15 17:21:57 +0800

    refactor(bubble-group): integrate PStickToBottom for improved scrolling behavior

--- src/components/bubble-group/index.vue
+++ src/components/bubble-group/index.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 import type { VirtualListOptions } from '../../composables/use-virtual-list'
 import type { BubbleGroupProps } from './types'
-import { nextTick, onMounted, shallowRef, watch } from 'vue'
-import { useStickToBottom } from '../../composables/use-stick-to-bottom.js'
+import { computed, nextTick, onMounted, shallowRef, watch } from 'vue'
 import { useVirtualList } from '../../composables/use-virtual-list.js'
 import PBacktop from '../backtop/index.vue'
+import PStickToBottom from '../stick-to-bottom/index.vue'
 
 defineOptions({
   name: 'PBubbleGroup',
@@ -13,16 +13,22 @@ defineOptions({
 
 const props = defineProps<BubbleGroupProps>()
 
-const containerRef = shallowRef<HTMLElement>()
-const contentRef = shallowRef<HTMLElement>()
+const stickRef = shallowRef<InstanceType<typeof PStickToBottom>>()
 
 const { totalSize, virtualItems, measureElement } = useVirtualList(
-  containerRef,
+  () => stickRef.value?.containerRef ?? null,
   props as unknown as VirtualListOptions,
 )
 
-const { stickIfNeeded, forceStickToBottom } = useStickToBottom(containerRef, contentRef, {
-  threshold: 8,
+const contentStyle = computed(() => {
+  if (!props.listData) {
+    return undefined
+  }
+
+  return {
+    height: `${totalSize.value}px`,
+    containIntrinsicSize: `auto ${totalSize.value}px`,
+  }
 })
 
 watch(
@@ -33,50 +39,47 @@ watch(
     }
 
     void nextTick(() => {
-      stickIfNeeded()
+      stickRef.value?.stickIfNeeded()
     })
   },
 )
 
 onMounted(() => {
-  forceStickToBottom()
+  stickRef.value?.forceStickToBottom()
 })
 </script>
 
 <template>
-  <div
-    ref="containerRef"
-    class="pxd-bubble-group relative overflow-x-hidden overflow-y-auto"
+  <PStickToBottom
+    ref="stickRef"
+    class="pxd-bubble-group relative overflow-x-hidden"
+    :threshold="8"
+    content-class="pxd-bubble-group--content relative w-full content-visibility-auto"
+    :content-style="contentStyle"
     v-bind="$attrs"
   >
     <template v-if="listData">
       <div
-        ref="contentRef"
-        class="pxd-bubble-group--content relative w-full content-visibility-auto"
-        :style="{ height: `${totalSize}px`, containIntrinsicSize: `auto ${totalSize}px` }"
+        v-for="virtualItem in virtualItems"
+        :key="virtualItem.key"
+        :ref="(el: any) => measureElement(el)"
+        :data-index="virtualItem.index"
+        class="pxd-bubble-group--item sibling:pbs-2 left-0 top-0 absolute w-full"
+        :style="{ transform: `translateY(${virtualItem.start}px)` }"
       >
-        <div
-          v-for="virtualItem in virtualItems"
-          :key="virtualItem.key"
-          :ref="(el: any) => measureElement(el)"
-          :data-index="virtualItem.index"
-          class="pxd-bubble-group--item sibling:pbs-2 left-0 top-0 absolute w-full"
-          :style="{ transform: `translateY(${virtualItem.start}px)` }"
-        >
-          <slot name="item" :item="listData[virtualItem.index]" :index="virtualItem.index" />
-        </div>
+        <slot name="item" :item="listData[virtualItem.index]" :index="virtualItem.index" />
       </div>
     </template>
 
     <slot v-else />
 
-    <div class="pxd-bubble-group--action bottom-0 h-0 sticky">
+    <template #action>
       <PBacktop
         class="bottom-1 shadow-sm left-1/2 z-1 w-max -translate-x-1/2 rounded-full"
         scroll-target="bottom"
         :append-to-body="false"
         scroll-behavior="instant"
       />
-    </div>
-  </div>
+    </template>
+  </PStickToBottom>
 </template>
