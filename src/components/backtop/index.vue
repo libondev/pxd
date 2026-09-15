1440c199ee77bb46040a1deead04b2fc88d96314
Libon <bon.li@outlook.com>
2026-09-15 17:02:37 +0800

    style(backtop): default btn size

--- src/components/backtop/index.vue
+++ src/components/backtop/index.vue
@@ -2,9 +2,9 @@
 import type { BacktopProps, BacktopEmits } from './types'
 import ArrowUpIcon from '@gdsicon/vue/arrow-up'
 import { computed, onBeforeUnmount, onMounted, shallowRef } from 'vue'
-import { useTailwindVariant } from '../../composables/_internal/use-tailwind-variant'
-import { getScrollTarget, getScrollElement } from '../../utils/dom'
-import { cachedOff, cachedOn, throttleByRaf } from '../../utils/event'
+import { useTailwindVariant } from '../../composables/_internal/use-tailwind-variant.js'
+import { getScrollTarget, getScrollElement } from '../../utils/dom.js'
+import { cachedOff, cachedOn, throttleByRaf } from '../../utils/event.js'
 import PButton from '../button/index.vue'
 import PTeleport from '../teleport/index.vue'
 
@@ -108,11 +108,8 @@ onBeforeUnmount(() => {
         @click="onActionClick"
       >
         <slot>
-          <PButton class="" shape="rounded" icon>
-            <ArrowUpIcon
-              class="pointer-events-none"
-              :class="{ 'rotate-180': scrollTarget === 'bottom' }"
-            />
+          <PButton size="sm" shape="rounded" icon>
+            <ArrowUpIcon :class="{ 'rotate-180': scrollTarget === 'bottom' }" />
           </PButton>
         </slot>
       </div>
